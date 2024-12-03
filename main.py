6b1469c80d941c672fe00f385be8cd811affc345
Alexander Steinmaurer <a@steinmaurer.cc>
2024-12-03 11:56:50 +0100

    Adding a main.py with all the functionality for the calculator.

---
+++ main.py
@@ -0,0 +1,17 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+
+print("Welcome to my calculator!")
+
+a = input("Enter a number: ")
+b = input("Enter another number: ")
+
+a = int(a)
+b = int(b)
+
+print(add(a, b))
+
