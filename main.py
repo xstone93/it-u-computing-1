4d0dd14f7c9f033791332f45df39e23fe27d2588
xstone93 <42343821+xstone93@users.noreply.github.com>
2024-12-03 12:00:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ def subtract(a, b):
 def mult(a, b):
     return a * b
 
+def dev(a, b):
+    if(b != 0):
+        return a / b
+
 print("Welcome to my calculator!")
 
 a = input("Enter a number: ")
